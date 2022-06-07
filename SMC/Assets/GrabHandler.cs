3ce1cd686a7e0c33ec6a822d2f2c8285f87203b6
pyuan-21 <yuanputianatin@outlook.com>
2022-06-07 18:47:21 +0200

    First commit

---
+++ SMC/Assets/GrabHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrabHandler : MonoBehaviour
+{
+    public Transform pivotLeft;
+    public Transform pivotRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onGrab()
+    {
+        var controller = GetComponent<XRGrabInteractable>();
+        var node = controller.selectingInteractor.gameObject.GetComponent<XRController>().controllerNode;
+        var grabInter = GetComponent<XRGrabInteractable>();
+        if(node == XRNode.LeftHand)
+        {
+            grabInter.attachTransform = pivotLeft;
+        }
+        else
+        {
+            grabInter.attachTransform = pivotRight;
+        }
+        Debug.Log("node:" + node);
+    }
+
+    public void onDetach()
+    {
+        Debug.Log("onDetach");
+        var grabInter = GetComponent<XRGrabInteractable>();
+        grabInter.attachTransform = null;
+    }
+}
---
+++ SMC/Assets/Oculus Hands Physics/Scripts/HandPresence.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;    
+    private InputDevice targetDevice;
+    public Animator handAnimator;
+    private Rigidbody rb;
+    private bool isGrabing;
+
+    void Start()
+    {
+        TryInitialize();
+    }
+
+    void TryInitialize()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+
+        rb = transform.GetComponent<Rigidbody>();
+
+        isGrabing = false;
+    }
+
+    void UpdateHandAnimation()
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+            //Debug.Log("Trigger: " + triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void SwitchNextObject()
+    {
+        //todo
+    }
+
+    void ResetObjectPos()
+    {
+        //todo
+    }
+
+    void HandleDeviceInput()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryValue);
+        targetDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryValue);
+        if (!isGrabing && primaryValue)
+        {
+            Debug.Log("primaryButton click");
+            SwitchNextObject();
+        }
+
+        if (!isGrabing && secondaryValue)
+        {
+            Debug.Log("secondaryButton click");
+            ResetObjectPos();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!targetDevice.isValid)
+        {
+            TryInitialize();
+        }
+        else
+        {
+            UpdateHandAnimation();
+            HandleDeviceInput();
+        }
+    }
+
+    public void OnGrabObject()
+    {
+        isGrabing = true;
+        if (rb != null)
+        {
+            rb.detectCollisions = false;
+            Debug.Log("OnGrabObject, rb.detectCollisions: " + rb.detectCollisions);
+        }
+    }
+
+    public void OnDetachObject()
+    {
+        isGrabing = false;
+        if (rb != null)
+        {
+            rb.detectCollisions = true;
+            Debug.Log("OnDetachObject, rb.detectCollisions: " + rb.detectCollisions);
+        }
+    }
+}
